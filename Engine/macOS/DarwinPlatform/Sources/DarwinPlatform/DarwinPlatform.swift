a7d066001a95c63e8746912e03fda93dde2151a7
Shota Shimazu <hornet.live.mf@gmail.com>
2017-10-25 00:52:18 +0900

    Update Darwin platform manager.

--- Engine/macOS/DarwinPlatform/Sources/DarwinPlatform/DarwinPlatform.swift
+++ Engine/macOS/DarwinPlatform/Sources/DarwinPlatform/DarwinPlatform.swift
@@ -1,4 +1,24 @@
 import Foundation
 
 #if os(macOS)
+
 #endif
+
+
+class PlatformDetector {
+
+    static func getPlatformWithVersion() -> String {
+
+        if #available(iOS 9.0, *) {
+            return "iOS9.0"
+        }
+        
+        if #available(iOS 10.0, *) {
+            return "iOS10.0"
+        }
+
+        if #available(iOS 11.0, *) {
+            return "iOS11.0"
+        }
+    }
+}
